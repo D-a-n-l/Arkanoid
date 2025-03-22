8876d79c6b7238638fac4108062fe7cd5bbd17bb
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-22 21:48:54 +0300

    Added in LevelConfigInstaller instantiate platform and ball

--- Assets/Arkanoid/Scripts/Installers/LevelConfigInstaller.cs
+++ Assets/Arkanoid/Scripts/Installers/LevelConfigInstaller.cs
@@ -1,3 +1,4 @@
+using MiniIT.Core;
 using MiniIT.Level;
 using UnityEngine;
 using Zenject;
@@ -12,6 +13,14 @@ namespace MiniIT.Installers
         public override void InstallBindings()
         {
             Container.Bind<LevelConfig>().FromInstance(levelConfig);
+
+            Movable platform = Container.InstantiatePrefab(levelConfig.Platform.Prefab, levelConfig.Platform.Position, Quaternion.Euler(levelConfig.Platform.Rotation), null).GetComponent<Movable>();
+
+            Container.Bind<Movable>().FromInstance(platform);
+
+            Bouncable ball = Container.InstantiatePrefab(levelConfig.Ball.Prefab, levelConfig.Ball.Position, Quaternion.Euler(levelConfig.Ball.Rotation), platform.transform).GetComponent<Bouncable>();
+
+            Container.Bind<Bouncable>().FromInstance(ball);
         }
     }
 }
