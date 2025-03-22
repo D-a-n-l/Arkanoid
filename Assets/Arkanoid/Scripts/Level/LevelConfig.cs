9b40a4ccd1f42b54cd7db530a2d9a05c935b663a
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-22 19:11:12 +0300

    Created Level Config

---
+++ Assets/Arkanoid/Scripts/Level/LevelConfig.cs
@@ -0,0 +1,35 @@
+using MiniIT.Core;
+using NaughtyAttributes;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level Config", menuName = "Configs/Level")]
+public class LevelConfig : ScriptableObject
+{
+    [field: SerializeField]
+    public Movable           Platform { get; private set; } = null;
+
+    [field: SerializeField]
+    public Bouncable         Ball { get; private set; } = null;
+
+    [field: SerializeField]
+    public CrashablePreset[] Crashables { get; private set; } = new CrashablePreset[1];
+
+    [Button]
+    public void InitBlocks()
+    {
+        foreach (CrashablePreset crashable in Crashables)
+        {
+            Instantiate(crashable.Prefab, crashable.Position, Quaternion.Euler(crashable.Rotation));
+        }
+    }
+}
+
+[System.Serializable]
+public struct CrashablePreset
+{
+    public Crashable Prefab;
+
+    public Vector3   Position;
+
+    public Vector3   Rotation;
+}
