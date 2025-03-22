542a94e664b919d7f35811edef39f8497272f007
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-22 16:51:26 +0300

    Refactored Movable for worked on IInput

--- Assets/Arkanoid/Scripts/Core/Movable.cs
+++ Assets/Arkanoid/Scripts/Core/Movable.cs
@@ -1,41 +1,28 @@
 using UnityEngine;
+using Zenject;
 
-
-public class Movable : MonoBehaviour, IForcable
+namespace MiniIT.Core
 {
-    private void Update()
+    public class Movable : MonoBehaviour, IForcable
     {
-#if UNITY_ANDROID
-        if (Input.touchCount > 0)
+        [Inject]
+        private IInput input = null;
+
+        private void Update()
         {
-            Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-            transform.position = new Vector2(
-                Mathf.Clamp(touchPosition.x, -10, 10),
-                transform.position.y
-            );
+            transform.position = input.Position(transform);
         }
-#endif
-#if UNITY_EDITOR
-        if (Input.GetMouseButton(0))
+
+        #region IForcable
+        public Vector2 Force(Transform enterObject, Collision2D collision)
         {
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float x = enterObject.position.x - collision.transform.position.x;
+            float width = collision.collider.bounds.size.x / 2;
 
-            transform.position = new Vector2(touchPosition.x, transform.position.y);
+            float bounceAngle = x / width;
+
+            return new Vector2(bounceAngle, 1).normalized;
         }
-#endif
+        #endregion
     }
-
-    #region IForcable
-    public Vector2 Force(Transform enterObject, Collision2D collision)
-    {
-        float x = enterObject.position.x - collision.transform.position.x;
-        float width = collision.collider.bounds.size.x / 2;
-
-        float bounceAngle = x / width;
-
-        return new Vector2(bounceAngle, 1).normalized;
-    }
-    #endregion
 }
