31c3d6aadd0ed43f8e96758464851a9128a4bb87
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-22 19:37:02 +0300

    Updated Bootstrap (added spawner for management)

--- Assets/Arkanoid/Scripts/Level/Bootstrap.cs
+++ Assets/Arkanoid/Scripts/Level/Bootstrap.cs
@@ -5,12 +5,22 @@ namespace MiniIT.Level
 {
     public class Bootstrap : MonoBehaviour
     {
+        [Min(0)]
+        [SerializeField]
+        private float       delayBetweenSpawn = 1f;
+
         [Inject]
         private LevelConfig levelConfig = null;
+
+        private Spawner     spawner;
 
         private void Awake()
         {
             CheckerCountDestroyed checker = new CheckerCountDestroyed(levelConfig);
+
+            spawner = new Spawner(levelConfig, delayBetweenSpawn);
+
+            StartCoroutine(spawner.Start());
         }
     }
 }
