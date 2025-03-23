ce5a5220895121d36d43de02f01ff2ea9418f677
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-23 23:43:40 +0300

    Renamed LevelConfigInstaller to LevelInstaller

---
+++ Assets/Arkanoid/Scripts/Installers/LevelInstaller.cs
@@ -0,0 +1,26 @@
+using MiniIT.Core;
+using MiniIT.Level;
+using UnityEngine;
+using Zenject;
+
+namespace MiniIT.Installers
+{
+    public class LevelInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private LevelConfig levelConfig = null;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<LevelConfig>().FromInstance(levelConfig);
+
+            Movable platform = Container.InstantiatePrefabForComponent<Movable>(levelConfig.Platform.Prefab, levelConfig.Platform.Position, Quaternion.Euler(levelConfig.Platform.Rotation), null);
+
+            Container.Bind<Movable>().FromInstance(platform);
+
+            Bouncable ball = Container.InstantiatePrefabForComponent<Bouncable>(levelConfig.Ball.Prefab, levelConfig.Ball.Position, Quaternion.Euler(levelConfig.Ball.Rotation), platform.transform);
+
+            Container.Bind<Bouncable>().FromInstance(ball);
+        }
+    }
+}
