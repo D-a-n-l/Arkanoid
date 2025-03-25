3fc75ef5e103026531b6f03ac038cf461b09d558
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-25 11:44:20 +0300

    Updated MinMax

--- Assets/Arkanoid/Scripts/Support/MinMax.cs
+++ Assets/Arkanoid/Scripts/Support/MinMax.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 [System.Serializable]
 public struct MinMax
 {
-    [Range(-3f, 3f)]
+    [Range(0f, 3f)]
     public float Min;
 
-    [Range(-3f, 3f)]
+    [Range(0f, 3f)]
     public float Max;
 }
