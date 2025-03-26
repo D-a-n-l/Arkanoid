2559920f5b9bff86905f5637ae20490174ba704d
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-26 13:05:56 +0300

    Created CameraScaler.cs

---
+++ Assets/Arkanoid/Scripts/Camera/CameraScaler.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using MiniIT.Enums;
+
+public class CameraScaler : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 referenceResolution = new Vector2(1080, 1920);
+
+    [SerializeField]
+    private WorkingModeCamera mode = WorkingModeCamera.ConstantWidth;
+
+    [Range(0f, 1f)]
+    [SerializeField]
+    private float matchWidthOrHeight = 0.5f;
+
+    private Camera componentCamera = Camera.main;
+
+    private float targetAspect = 1;
+
+    private float cameraZoom = 1;
+
+    private float initialSize = 5;
+
+    private void Awake()
+    {
+        initialSize = componentCamera.orthographicSize;
+
+        targetAspect = referenceResolution.x / referenceResolution.y;
+
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        switch (mode)
+        {
+            case WorkingModeCamera.ConstantHeight:
+                componentCamera.orthographicSize = initialSize / cameraZoom;
+                break;
+
+            case WorkingModeCamera.ConstantWidth:
+                componentCamera.orthographicSize = initialSize * (targetAspect / componentCamera.aspect) / cameraZoom;
+                break;
+
+            case WorkingModeCamera.MatchWidthOrHeight:
+                float vSize = initialSize;
+                float hSize = initialSize * (targetAspect / componentCamera.aspect);
+
+                float vLog = Mathf.Log(vSize, 2);
+                float hLog = Mathf.Log(hSize, 2);
+
+                float logWeightedAverage = Mathf.Lerp(hLog, vLog, matchWidthOrHeight);
+
+                componentCamera.orthographicSize = Mathf.Pow(2, logWeightedAverage) / cameraZoom;
+                break;
+
+            case WorkingModeCamera.Expand:
+                if (targetAspect > componentCamera.aspect)
+                {
+                    componentCamera.orthographicSize = initialSize * (targetAspect / componentCamera.aspect) / cameraZoom;
+                }
+                else
+                {
+                    componentCamera.orthographicSize = initialSize / cameraZoom;
+                }
+
+                break;
+
+            case WorkingModeCamera.Shrink:
+                if (targetAspect < componentCamera.aspect)
+                {
+                    componentCamera.orthographicSize = initialSize * (targetAspect / componentCamera.aspect) / cameraZoom;
+                }
+                else
+                {
+                    componentCamera.orthographicSize = initialSize / cameraZoom;
+                }
+
+                break;
+
+            default:
+                componentCamera.orthographicSize = initialSize / cameraZoom;
+                break;
+        }
+    }
+}
